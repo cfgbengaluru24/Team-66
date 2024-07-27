379e4f98517fff57afdbe9b961aad38955a63fb0
kanika0304 <112932559+kanika0304@users.noreply.github.com>
2024-07-28 00:00:56 +0530

    i wish no conflict will occur

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,18 +1,12 @@
 import Login from "./components/loginschool/Login";
 import Navbar from "./components/Navbar/page";
 import Signup from "./components/Signupschool.tsx/signup";
-import Signupdonor from "./components/Signupdonor/Signupdonor";
-import LoginDonor from "./components/logindonor/Logindonor";
-import Inventory from "./components/inventory/inventory";
 
 export default function Home() {
   return (
     <>
       <div data-theme="light">
         <Navbar />
-        <Signupdonor />
-        <LoginDonor />
-        <Inventory />
         <section className="carousel w-full h-screen relative">
           {/* Carousel Items */}
           <div id="slide1" className="carousel-item relative w-full h-full">
