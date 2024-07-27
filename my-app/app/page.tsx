a0621bad8be2c46bebb101624048a022efb0da34
kanika0304 <112932559+kanika0304@users.noreply.github.com>
2024-07-28 01:04:54 +0530

    volunteer updated

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,12 +1,15 @@
 import Login from "./components/loginschool/Login";
 import Navbar from "./components/Navbar/page";
 import Signup from "./components/Signupschool.tsx/signup";
+import AddDetail from "./volunteer/adddetails/adddetails";
+
 
 export default function Home() {
   return (
     <>
       <div data-theme="light">
         <Navbar />
+        <AddDetail />
         <section className="carousel w-full h-screen relative">
           {/* Carousel Items */}
           <div id="slide1" className="carousel-item relative w-full h-full">
