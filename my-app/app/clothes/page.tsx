c8d5a8a6e89b2c63a36fad8085b63ac854e752b8
kanika0304 <112932559+kanika0304@users.noreply.github.com>
2024-07-28 04:48:41 +0530

    Any conflict

--- my-app/app/clothes/page.tsx
+++ my-app/app/clothes/page.tsx
@@ -1,13 +1,11 @@
 import React from "react";
 import Navbar from "../components/Navbar/page";
-import VolunteerSurvey from "./volunteerclothes/volunteerclothes";
-import ClothingDonorPage from "./clothingdonor/clothingdonor";
+
 const page = () => {
   return (
     <div>
       <Navbar />
-      <VolunteerSurvey />
-      <ClothingDonorPage />
+     
     </div>
   );
 };
